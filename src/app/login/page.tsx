c850efefa6fa02f678497e5118374172842eda6d
Ajay Shakya <ajayshakya7860@gmail.com>
2025-06-08 12:04:20 +0530

    Site url dynamic added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -48,7 +48,7 @@ export default function LoginPage() {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `http://localhost:3000/auth/callback`,
+          redirectTo: `$(process.env.SITE_URL)/auth/callback?next=/dashboard`,
         },
       });
      
