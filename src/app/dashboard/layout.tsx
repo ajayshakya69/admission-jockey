8e8756715d464ceb0c65ece04c27bf3690883578
Ajay <ajayshakya7860@gmail.com>
2025-06-12 14:48:42 +0530

    fix: sidebar fixed chat updated responsive problem

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,6 +7,8 @@ import { useSupabase } from "@/services/supabase/supabase.hook";
 import { DropdownMenuDemo } from "./partials/userDropDown";
 import Loader from "@/components/ui/loader";
 import Sidebar from "./dropmenu/page";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/app/dashboard/partials/app-sidebar";
 
 export default function DashboardLayout({
   children,
@@ -22,54 +24,56 @@ export default function DashboardLayout({
   if (isLoading || !authorised) return <Loader />;
 
   return (
-    <div className="flex h-screen overflow-hidden bg-black text-white">
-      {/* Left Sidebar */}
-      <Sidebar />
+    <SidebarProvider>
+      <AppSidebar />
+      <div className="flex h-screen overflow-hidden bg-black text-white w-full">
+        {/* Left Sidebar */}
+        {/* <Sidebar /> */}
 
-      {/* Main content area */}
-      <div className="flex flex-col flex-1 overflow-hidden">
-        {/* Header */}
-        <header className="w-full bg-[#00000000] px-6 py-4">
-          <div className="flex items-center justify-end relative">
-            {/* Left logo area */}
-            <div className="lg:hidden flex items-center absolute top-2 left-22">
-              <img src="/logo.jpg" alt="logo" className="h-10 w-auto" />
-            </div>
-            {/* Desktop header controls */}
-            <div className="hidden md:flex items-center gap-4">
-              {/* <Button
-                className="px-6 py-2 rounded-lg font-semibold text-white text-base
+        {/* Main content area */}
+        <div className="flex flex-col flex-1 overflow-hidden">
+          {/* Header */}
+          <header className="w-full bg-[#00000000] px-6 py-4">
+            {/* <SidebarTrigger /> */}
+            <div className="flex items-center justify-end relative">
+              {/* Left logo area */}
+              <div className=" flex items-center absolute top-2 left-0">
+                <SidebarTrigger className=" w-10 h-10 rounded-full bg-[linear-gradient(90deg,#A07DF1,#F69DBA)] text-white flex items-center justify-center shadow-md" />
+              </div>
+              {/* Desktop header controls */}
+              <div className="hidden md:flex items-center gap-4">
+                {/* <Button
+                  className="px-6 py-2 rounded-lg font-semibold text-white text-base
                bg-[linear-gradient(90deg,#A07DF1,#F69DBA)]
                border-t border-[#ffffff36]
                shadow-[0_0_11px_0_rgba(160,125,241,0.3)]
                hover:opacity-90 transition-all duration-300 flex items-center gap-2"
-              >
-                <span className="hidden sm:block">New Chat</span>
-              </Button> */}
+                >
+                  <span className="hidden sm:block">New Chat</span>
+                </Button> */}
 
-              <button className="rounded-full flex items-center justify-center h-10 w-10 p-[1px] bg-[linear-gradient(90deg,#A07DF1,#F69DBA)]">
-                <div className="rounded-full h-full w-full bg-black flex justify-center items-center">
-                  <BellRing className="h-[18px] text-[#f2acc3]" />
+                <button className="rounded-full flex items-center justify-center h-10 w-10 p-[1px] bg-[linear-gradient(90deg,#A07DF1,#F69DBA)]">
+                  <div className="rounded-full h-full w-full bg-black flex justify-center items-center">
+                    <BellRing className="h-[18px] text-[#f2acc3]" />
+                  </div>
+                </button>
+
+                <div className="rounded-full h-10 w-10 bg-[linear-gradient(90deg,#A07DF1,#F69DBA)] flex items-center justify-center">
+                  <DropdownMenuDemo />
                 </div>
-              </button>
+              </div>
 
-              <div className="rounded-full h-10 w-10 bg-[linear-gradient(90deg,#A07DF1,#F69DBA)] flex items-center justify-center">
-                <DropdownMenuDemo />
+              {/* Mobile dropdown */}
+              <div className="md:hidden">
+                <UserDropDown />
               </div>
             </div>
+          </header>
 
-            {/* Mobile dropdown */}
-            <div className="md:hidden">
-              <UserDropDown />
-            </div>
-          </div>
-        </header>
-
-        {/* Page Content */}
-        <main className="flex-1 overflow-y-hidden px-6 py-4 hide-scroll">
-          {children}
-        </main>
+          {/* Page Content */}
+          <main className="flex-1 overflow-y-auto px-6 py-2">{children}</main>
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}", // adjust based on your project structure
   ],
   theme: {
     extend: {
-        backgroundImage: {
-            'theme-gradient': 'linear-gradient(180deg, rgba(232, 125, 179, 0.5) 0%, rgba(140, 103, 226, 0.5) 100%)',
-          },
+      backgroundImage: {
+        "theme-gradient":
+          "linear-gradient(180deg, rgba(232, 125, 179, 0.5) 0%, rgba(140, 103, 226, 0.5) 100%)",
+      },
     },
   },
   plugins: [
