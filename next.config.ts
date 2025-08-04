65e5e62bfa01c4010b11d13d492919e40bc21923
Ajay <ajayshakya7860@gmail.com>
2025-08-04 16:40:20 +0530

    dash updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // allow any HTTPS domain
+      },
+      {
+        protocol: "http",
+        hostname: "**", // allow any HTTP domain (less secure, use only if needed)
+      },
+    ],
+  },
 };
 
 export default nextConfig;
