593323a03820578d3bb0549d1b10c2d4df3748b7
Ajay <ajayshakya7860@gmail.com>
2025-08-07 20:37:02 +0530

    code reverted

--- src/app/dashboard/collegefinder/page.tsx
+++ src/app/dashboard/collegefinder/page.tsx
@@ -1,30 +1,30 @@
-"use client"
+"use client";
 
-import { CollegeCard } from "./partials/college-card"
-import { MoreInstitutions } from "./partials/more-institutions"
-import { colleges } from "./partials/data"
-
+import { CollegeCard } from "./partials/college-card";
+import { MoreInstitutions } from "./partials/more-institutions";
+import { colleges } from "./partials/data";
 
 export default function CollegesPage() {
   return (
     <div className="max-h-screen dark:text-white text-black dark:bg-gradient-b dark:from-[#000000b0] dark:via-[#000000b0] dark:to-[#000000b0] bg-gradient-to-b from-[#F6F6F6] via-[#fef4f7] to-[#efeafe] overflow-y-scroll hide-scroll">
       <div className="container mx-auto p-4 lg:px-0 lg:py-10 max-w-6xl">
-      <div className="text-center mb-12">
-        <h1 className="text-[18px] md:text-[24px] lg:text-[32px] font-bold text-center text-black dark:text-white mb-4">
-        Your Gateway to India's Best <br className="lg:hidden block" />Colleges
-        </h1>
-      </div>
+        <div className="text-center mb-12">
+          <h1 className="text-[18px] md:text-[24px] lg:text-[32px] font-bold text-center text-black dark:text-white mb-4">
+            Your Gateway to India's Best <br className="lg:hidden block" />
+            Colleges
+          </h1>
+        </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
-        {colleges.map((college) => (
-        <CollegeCard key={college.id} college={college} />
-        ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
+          {colleges.map((college) => (
+            <CollegeCard key={college.id} college={college} />
+          ))}
 
-        <div className="md:col-span-1 lg:col-span-1">
-        <MoreInstitutions />
+          <div className="md:col-span-1 lg:col-span-1">
+            <MoreInstitutions />
+          </div>
         </div>
       </div>
-      </div>
     </div>
-  )
+  );
 }
---
+++ src/app/providers/colleges/college.provider.tsx
@@ -0,0 +1,34 @@
+"use client ";
+
+import { ReactNode } from "react";
+import { CollegeContext } from "./college.context";
+import { useAxios } from "@/services/axios/axios.hook";
+import { useQuery } from "@tanstack/react-query";
+import { PropertyApiResponse } from "./colleget.types";
+
+export function CollegeProvider({ children }: { children: ReactNode }) {
+  const { axios } = useAxios();
+
+  const {
+    data: colleges,
+    isLoading,
+    refetch: refectColleges,
+  } = useQuery({
+    queryKey: ["colleges"],
+    queryFn: async () => {
+      const data = await axios.get<PropertyApiResponse>(
+        `${process.env.NEXT_PUBLIC_API_URL}/property`,
+      );
+      console.log(data);
+      return data.data;
+    },
+  });
+
+  return (
+    <CollegeContext.Provider
+      value={{ colleges: colleges ?? [], isLoading, refectColleges }}
+    >
+      {children}
+    </CollegeContext.Provider>
+  );
+}
